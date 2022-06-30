c6dfabb06a4e7b092ecde396006d7c3ae8dacb71
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-06-30 12:08:30 -0700

    Create colors.py

---
+++ Unpro/colors.py
@@ -0,0 +1,9 @@
+class bcolors:
+    CYAN = '\u001b[38;5;51;1m'
+    GREEN = '\033[0;32m'
+    RED = '\033[91m'
+    BLUE = '\u001b[34m'
+    YELLOW = '\u001b[38;5;226m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
