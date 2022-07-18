8b2e3c32ab32fd40ef668e7b8d326af2cd510033
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-07-17 21:27:14 -0700

    Update oreprices.sh
    cleaned tabs-to-spaces. Added cd into directory due to error of opening files in local directory when outside of it.

--- EveOrePrices/oreprices.sh
+++ EveOrePrices/oreprices.sh
@@ -1,16 +1,17 @@
 #!/bin/zsh
 
 while [ $# -gt 0 ]; do
-	for input in "$@" ; do
-		case $input in
-			shortlist)
-				echo arkonor bistot crokite dark_ochre gneiss hedbergite hemorphite jaspet kernite mercoxit omber plagioclase pyroxeres scordite spodumain veldspar bitumens carnotite chromite cinnabar cobaltite coesite euxenite loparite monazite otavite pollucite scheelite sperrylite sylvite titanite vanadinite xenotime ytterbite zeolites zircon blue_ice clear_icicle dark_glitter gelidus glacial_mass glare_crust krystallos white_glaze update
-			;;
-			*)
-				python3 /home/user/Python/Projects/EveOrePrices/main.py "$@"
-				exit
-			;;
-		esac
-		shift
-	done
+  for input in "$@" ; do
+    case $input in
+      shortlist)
+        echo arkonor bistot crokite dark_ochre gneiss hedbergite hemorphite jaspet kernite mercoxit omber plagioclase pyroxeres scordite spodumain veldspar bitumens carnotite chromite cinnabar cobaltite coesite euxenite loparite monazite otavite pollucite scheelite sperrylite sylvite titanite vanadinite xenotime ytterbite zeolites zircon blue_ice clear_icicle dark_glitter gelidus glacial_mass glare_crust krystallos white_glaze update
+      ;;
+      *)
+        cd $HOME/Python/Projects/EveOrePrices/
+        python3 main.py "$@"
+	exit
+      ;;
+    esac
+  shift
+  done
 done
