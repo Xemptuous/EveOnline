1de78200f1b7f5d01036da4069f0b2420a065f4c
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-07-25 21:12:48 -0700

    Update oreprices-completion.sh
    added triglavian ores to autocomplete

--- EveOrePrices/oreprices-completion.sh
+++ EveOrePrices/oreprices-completion.sh
@@ -6,7 +6,7 @@ _autocompleteOres()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="arkonor bistot crokite dark_ochre gneiss hedbergite hemorphite jaspet kernite mercoxit omber plagioclase pyroxeres scordite spodumain veldspar bitumens carnotite chromite cinnabar cobaltite coesite euxenite loparite monazite otavite pollucite scheelite sperrylite sylvite titanite vanadinite xenotime ytterbite zeolites zircon blue_ice clear_icicle dark_glitter gelidus glacial_mass glare_crust krystallos white_glaze update"
+    opts="arkonor bezdnacine bistot crokite dark_ochre gneiss hedbergite hemorphite jaspet kernite mercoxit omber plagioclase pyroxeres rakovene scordite spodumain talassonite veldspar bitumens carnotite chromite cinnabar cobaltite coesite euxenite loparite monazite otavite pollucite scheelite sperrylite sylvite titanite vanadinite xenotime ytterbite zeolites zircon blue_ice clear_icicle dark_glitter gelidus glacial_mass glare_crust krystallos white_glaze update"
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
   return 0
 
