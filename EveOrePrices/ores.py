1482ded7ecc0d442f102508ae55e7467fbe44c10
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2022-08-08 22:28:46 -0700

    Update ores.py
    made changes to reflect argparse addition

--- EveOrePrices/ores.py
+++ EveOrePrices/ores.py
@@ -1,9 +1,9 @@
-import sys
+import json
 import classes
 
-def getOrePrices(allOres, allMinerals):
+def getOrePrices(args, allOres, allMinerals):
     finalList = []
-    for arg in sys.argv[1:]:
+    for arg in args:
         try:
             parent = allOres[arg]
         except KeyError:
@@ -11,10 +11,8 @@ def getOrePrices(allOres, allMinerals):
             return 1
         tempList = []
         for ore in list(parent.keys()):
-            # initializing class for each ore in the parent category.
             Ore = classes.Ore(parent[ore])
             bonus = f"+{int(Ore.bonus * 100)}%"
-            # returning list of all minerals for this ore that have values.
             mineralsList = [[key, val] for key, val in Ore.minerals.items() if val]
             refinedPrice = "{:.2f}".format(getRefinedPrice(mineralsList, Ore.volume, allMinerals))
             bidAskPrice = [Ore.bidPrice, Ore.askPrice]
@@ -25,9 +23,16 @@ def getOrePrices(allOres, allMinerals):
 
 def getRefinedPrice(mineralList, volume, mineralData):
     total = 0
+    refine = getRefineYield()
     for mineral, amount in mineralList:
-        # initializing class for current mineral.
+        amount = amount * refine
         Mineral = classes.Mineral(mineralData[mineral])
         avg_price = (Mineral.bidPrice + Mineral.askPrice) / 2
         total += ((amount / volume) * avg_price * volume) if volume < 1 else ((amount * volume) * avg_price / volume)
     return round(total,2)
+
+
+def getRefineYield():
+    with open("options.json") as file:
+        options = json.load(file)
+        return options["refine"]
